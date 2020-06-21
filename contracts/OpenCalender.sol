126c48b2e7156b4adc15bf5a99b342ff3abb7cc1
Anjan Roy <anjanroy@yandex.com>
2020-06-21 18:58:38 +0530

    Started writing smart contract

---
+++ contracts/OpenCalender.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+contract OpenCalender {
+    address payable public author;
+
+    constructor() public {
+        author = msg.sender;
+    }
+}
